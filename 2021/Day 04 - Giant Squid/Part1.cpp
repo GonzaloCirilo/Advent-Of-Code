e3ce8dbfea7c3090acd4c5ceec138edc22d6f441
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2021-12-07 00:33:55 -0500

    Day 04 2021

---
+++ 2021/Day 04 - Giant Squid/Part1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool checkWinnerBoard(vector<bool> marks) {
+    int cols[5] = {}, rows[5] = {};
+    for(int i = 0; i < 5; i ++) {
+        for(int j = 0; j < 5; j++) {
+            int ind = i * 5 + j;
+            rows[i] += marks[ind];
+        }
+        if(rows[i] == 5){
+            return true;
+        }
+        for(int j = 0; j < 5; j++) {
+            int ind = i + j * 5;
+            cols[i] += marks[ind];
+        }
+        if(cols[i] == 5){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    string take, l; int n; char c;
+    vector<int> arr;
+    while(scanf("%d%c", &n, &c) && c != '\n') {
+        arr.push_back(n);
+    }
+
+    vector<vector<int>> boards = vector<vector<int>>();
+    vector<vector<bool>> marks = vector<vector<bool>>();
+    int x, b = 0, cont = 0;
+    vector<int> board = vector<int>();
+    while(cin >> x){
+        board.push_back(x);
+        cont++;
+        if (cont == 25) {
+            cont = 0;
+            boards.push_back(board);
+            marks.push_back(vector<bool>(25, false));
+            board.clear();
+        }
+    }
+
+    bool found = false;
+    for(int i = 0; i < arr.size(); i++) {
+        int n = arr[i];
+
+        for(int j = 0; j < boards.size(); j++){
+            vector<int>::iterator p = find(boards[j].begin(), boards[j].end(), n);
+            if (p != boards[j].end()){
+                int ind = p - boards[j].begin();
+                marks[j][ind] = true;
+            }
+
+            if(checkWinnerBoard(marks[j])){
+                int sum = 0;
+                for(int k = 0; k < marks[j].size(); k++){
+                    if(!marks[j][k]){
+                        sum += boards[j][k];
+                    }
+                }
+                cout << sum * n;
+                found = true;
+                break;
+            }
+        }
+        if(found)
+            break;
+    }
+    return 0;
+}
