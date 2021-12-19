ace99186946f136af2cca8ccd0f626ac4416ebac
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2021-12-18 23:10:50 -0500

    Day 06 2020

---
+++ 2020/Day 06 - Custom Customs/Part2.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdin = std.io.getStdOut().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    var file = try std.fs.cwd().openFile("in6.txt", .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+    var buf: [1024]u8 = undefined;
+
+    var dict: [26]i32 = [_]i32{0} ** 26;
+    var ans: i32 = 0; var part: i32 = 0;
+    var prev: bool = false;
+    while(try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try stdout.print("{s}\n", .{line});
+
+
+        if(line.len == 0){
+            if(prev) break;
+            var aux: i32 = 0;
+            for(dict) |count|{
+                if(count == part){
+                    aux += 1;
+                }
+            }
+            ans += aux;
+            prev = true;
+            part = 0;
+            dict = [_]i32{0} ** 26;
+        }else{
+            part += 1;
+            prev = false;
+            for(line) |c| {
+                dict[c-'a'] += 1;
+            }
+        }
+    }
+
+    try stdout.print("{}\n", .{ans});
+}
