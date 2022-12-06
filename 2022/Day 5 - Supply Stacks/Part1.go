296068998909413a2bb3f0eb146f2dd5d979c982
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2022-12-05 19:09:42 -0500

    Day 5, Part 1 2022

---
+++ 2022/Day 5 - Supply Stacks/Part1.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"container/list"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	stacks := make([]list.List, 9)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) <= 0 {
+			continue
+		}
+		if line[1] == '1' {
+			continue
+		} else if line[0] == 'm' {
+			var amount, from, to int
+			fmt.Sscanf(line, "move %d from %d to %d", &amount, &from, &to)
+			from--
+			to--
+			for i := 0; i < amount; i++ {
+				ele := stacks[from].Back()
+				stacks[to].PushBack(ele.Value)
+				stacks[from].Remove(ele)
+			}
+		} else {
+			for i := 0; i+1+i*3 < len(line); i++ {
+				ind := i + 1 + i*3
+				if line[ind] != ' ' {
+					stacks[i].PushFront(line[ind])
+				}
+			}
+		}
+	}
+	for _, s := range stacks {
+		if s.Back() != nil {
+			fmt.Printf("%c", s.Back().Value)
+		}
+
+	}
+}
