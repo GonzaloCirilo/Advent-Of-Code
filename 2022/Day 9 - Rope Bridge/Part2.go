9b7fac3d7182008f0d45882fe8952098cb22b10c
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2022-12-09 23:33:43 -0500

    Day 9, Part 2 2022

--- 2022/Day 9 - Rope Bridge/Part2.go
+++ 2022/Day 9 - Rope Bridge/Part2.go
@@ -13,27 +13,20 @@ type void struct{}
 var member void
 
 func updatePosition(tx, ty, hx, hy int) (int, int) {
+	var dx, dy []int
 	//fmt.Println("prev Up", tx, ty, hx, hy)
 	if tx == hx || ty == hy {
-		dx := []int{1, 0, 0, -1}
-		dy := []int{0, -1, 1, 0}
-		if math.Abs(float64(hx)-float64(tx)) >= 2 || math.Abs(float64(hy)-float64(ty)) >= 2 {
-			for i := range dx {
-				auxX, auxY := tx+dx[i], ty+dy[i]
-				if math.Abs(float64(hx)-float64(auxX)) < 2 && math.Abs(float64(hy)-float64(auxY)) < 2 {
-					return auxX, auxY
-				}
-			}
-		}
+		dx = []int{1, 0, 0, -1}
+		dy = []int{0, -1, 1, 0}
 	} else {
-		dx := []int{1, 1, -1, -1}
-		dy := []int{1, -1, -1, 1}
-		if math.Abs(float64(hx)-float64(tx)) >= 2 || math.Abs(float64(hy)-float64(ty)) >= 2 {
-			for i := range dx {
-				auxX, auxY := tx+dx[i], ty+dy[i]
-				if math.Abs(float64(hx)-float64(auxX)) < 2 && math.Abs(float64(hy)-float64(auxY)) < 2 {
-					return auxX, auxY
-				}
+		dx = []int{1, 1, -1, -1}
+		dy = []int{1, -1, -1, 1}
+	}
+	if math.Abs(float64(hx)-float64(tx)) >= 2 || math.Abs(float64(hy)-float64(ty)) >= 2 {
+		for i := range dx {
+			auxX, auxY := tx+dx[i], ty+dy[i]
+			if math.Abs(float64(hx)-float64(auxX)) < 2 && math.Abs(float64(hy)-float64(auxY)) < 2 {
+				return auxX, auxY
 			}
 		}
 	}
