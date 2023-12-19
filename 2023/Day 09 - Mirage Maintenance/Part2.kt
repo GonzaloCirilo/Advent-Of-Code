33a9744b4251a00e3b34cd8148e0d4810de0d256
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2023-12-18 20:30:55 -0500

    Day 10, 2023

---
+++ 2023/Day 09 - Mirage Maintenance/Part2.kt
@@ -0,0 +1,20 @@
+fun isAllZeroes(arr: List<Long>): Boolean {
+	return arr.reduce { acc, x -> acc + x} == 0L
+}
+
+fun solve(arr: List<Long>): Long {
+	if (isAllZeroes(arr)) return arr[0]
+	val nArr = mutableListOf<Long>()
+	arr.dropLast(1).forEachIndexed { i, _ ->
+		nArr.add(arr[i + 1] - arr[i])
+	}
+	return arr.first() - solve(nArr)
+}
+
+
+fun main() {
+	val ans = generateSequence(::readLine).toList().map { it.split(" ").map { x -> x.toLong() } }.map {  series ->
+		solve(series)
+	}.reduce { acc, s -> acc + s}
+	println(ans)
+}
